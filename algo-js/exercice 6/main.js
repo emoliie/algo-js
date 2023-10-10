147c45e139dce9233f3837920d4168ca1c4b03b2
emoliie <147377815+emoliie@users.noreply.github.com>
2023-10-10 11:09:41 +0200

    Add files via upload

---
+++ algo-js/exercice 6/main.js
@@ -0,0 +1,14 @@
+let a = "Jean";
+let b = "Paul";
+
+function checkName(name1, name2) {
+  if (name1 == name2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let result = checkName(a, b);
+
+console.log(result);
